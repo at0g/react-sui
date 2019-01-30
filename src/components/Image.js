59d6bca9d4f02dfcb9ecd731e89e5f8d94097ff2
Ben Daley <benjamindaley@gmail.com>
2019-01-31 02:42:06 +1100

    use inline svg

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,60 +1,72 @@
 import React from 'react'
 import styled from 'styled-components'
 import ImagePreloader from '../containers/ImagePreloader'
-import fallbackImg from '../assets/images/fallback-img.svg'
+import FallbackImg from './images/FallbackImg'
 
 const paddingBottom = ({ width, height }) => `${height / width * 100}%`
 
-const StyledImg = styled('div')`
+const Container = styled('div')`
     display: inline-block;
     width: ${p => p.width / 16}rem;
     max-width: 100%;
+    position: relative;
+    user-select: none;
     &:before {
         content: "";
-        background: transparent url("${p => p.src}") 50% 50% no-repeat;
-        background-size: cover; 
         box-sizing: content-box;
         display: block;
         height: 0;
         padding: 0 0 ${paddingBottom};
-        max-width: 100%;
+        position: relative;
+        max-width: 100%;        
     }
 `
 
-function Image({ src, fallback, width, height, scale, ...props }) {
+const Display = styled('div')`
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    svg& {
+        fill: currentColor;
+        pointer-events: none;
+    }
+`
+
+const StyledImg = styled('div')`
+    background: transparent url("${p => p.src}") 50% 50% no-repeat;
+    background-size: cover;
+`
+
+const Spinner = () => <span>Loading</span>
+
+function Image({ src, width, height, scale, ...props }) {
     return (
         <ImagePreloader src={src}>
             {({ loaded, error }) => {
-                const showLoading = !loaded && !error
-                const showFallback = loaded && error
-                const showImage = loaded || showFallback
-
-                if (showLoading) {
-                    return (
-                        <span>Loading</span>
-                    )
-                }
-
                 const displayWidth = Math.floor(width / scale)
                 const displayHeight = Math.floor(height / scale)
 
-                if (showImage) {
-                    return (
-                        <StyledImg
-                            src={showFallback ? fallback : src}
-                            width={displayWidth}
-                            height={displayHeight}
-                            aria-hidden="true"
-                        />
-                    )
+                let Component = Spinner
+                if (loaded && error) {
+                    Component = FallbackImg
                 }
+                else if (loaded && !error) {
+                    Component = StyledImg
+                }
+
+                return (
+                    <Container aria-hidden="true" width={displayWidth} height={displayHeight}>
+                        <Display as={Component} src={src} />
+                    </Container>
+                )
             }}
         </ImagePreloader>
     )
 }
 
 Image.defaultProps = {
-    fallback: fallbackImg,
     scale: window.devicePixelRatio || 1
 }
 
