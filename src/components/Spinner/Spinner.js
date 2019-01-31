2225601df71699d2cb385ffd7b4b12a88df7a5e4
Ben Daley <benjamindaley@gmail.com>
2019-02-01 10:08:31 +1100

    Rename project to react-sui

---
+++ src/components/Spinner/Spinner.js
@@ -0,0 +1,45 @@
+import React from 'react'
+
+class Spinner extends React.Component {
+    static degreesToRadians(degrees) {
+        return degrees * Math.PI / 180
+    }
+
+    componentDidMount() {
+        this.animate()
+    }
+
+    componentWillUnmount() {
+        window.cancelAnimationFrame(this.animationFrameId)
+    }
+
+    animate = () => {
+        if (this.canvas) {
+            this.draw(this.canvas.getContext('2d'), this.props.size / 2)
+        }
+        this.animationFrameId = window.requestAnimationFrame(this.animate)
+    }
+
+    draw = (ctx, radius) => {
+        const rotationInDegrees = (new Date().getTime()) % 360
+        const startAngle =  this.constructor.degreesToRadians(rotationInDegrees)
+        const endAngle = this.constructor.degreesToRadians(rotationInDegrees + 90)
+        ctx.clearRect(0, 0, radius * 2, radius * 2)
+        ctx.beginPath()
+        ctx.arc(radius, radius, radius - 2, startAngle, endAngle)
+        ctx.stroke()
+    }
+
+    render() {
+        const { size } = this.props;
+        return (
+            <canvas
+                ref={el => this.canvas = el}
+                width={size}
+                height={size}
+            />
+        )
+    }
+}
+
+export default Spinner
---
+++ src/components/Spinner/Spinner.stories.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import { number } from '@storybook/addon-knobs'
+import Spinner from './Spinner'
+
+storiesOf('Atoms/Spinner', module)
+    .add('basic example', () => (
+        <Spinner
+            size={number('size', 48, 'props')}
+        />
+    ))
